f35c6ae17ab41be054a31daacddeed9ba4adcaac
Etienne LC <etlecoent@gmail.com>
2021-01-27 18:03:56 -0500

    Imported index router

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,25 @@
+// Express setup
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const port = process.env.PORT || 3001;
 
+// Database setup
+const db = require("./db/knexfile");
+// const dbHelpers = require("./helpers/dbHelpers")(db);
+
+// Environment setup
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
   const logger = require("morgan");
   app.use(logger("dev"));
 }
 
-const knex = require("knex")({
-  client: "pg",
-  version: "7.2",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// Routes setup
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
