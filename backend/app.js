a5298753a234fcc8df12cb9e3236dda9506aeb0d
Etienne LC <etlecoent@gmail.com>
2021-01-27 16:34:00 -0500

    Added requiring variables from .env file

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const app = express();
 const port = process.env.PORT || 3001;
 
