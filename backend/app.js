7b788f58594235ffc1888a868d0e5a51776f93f6
Etienne LC <etlecoent@gmail.com>
2021-01-27 16:50:44 -0500

    Required morgan, knex, bodyparser & urlencoded as middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 3001;
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
+  const logger = require("morgan");
+  app.use(logger("dev"));
 }
 
-const app = express();
-const port = process.env.PORT || 3001;
+const knex = require("knex")({
+  client: "pg",
+  version: "7.2",
+  connection: {
+    host: "127.0.0.1",
+    user: "your_database_user",
+    password: "your_database_password",
+    database: "myapp_test",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
