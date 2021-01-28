5e2f5b8b8a7eb6e7499f6923125e97c6c70e814e
Etienne LC <etlecoent@gmail.com>
2021-01-28 16:01:28 -0500

    Removed method responsible of getting a particular book for a particular bookstore

--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -42,17 +42,6 @@ module.exports = (db) => {
       .catch((err) => console.log(err));
   };
 
-  const getBookByIdForBookstoreById = (bookstore_id, book_id) => {
-    return db
-      .select("*")
-      .from("books")
-      .innerJoin("stored_books", "books.id", "stored_books.book_id")
-      .where("bookstore_id", bookstore_id)
-      .where("book_id", book_id)
-      .then((result) => result[0])
-      .catch((err) => console.log(err));
-  };
-
   return {
     getUsers,
     getBooks,
