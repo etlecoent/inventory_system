104d2eb4782d4a9e1551512378d2da8b3e1a4ae7
Etienne LC <etlecoent@gmail.com>
2021-01-27 20:09:44 -0500

    Fixed typo in bookstores migration & added books migration

--- backend/db/migrations/20210127200059_create_stored_books.js
+++ backend/db/migrations/20210127200059_create_stored_books.js
@@ -1,12 +1,14 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("bookstores", (table) => {
+  return knex.schema.createTable("books", (table) => {
     table.increments("id");
-    table.string("name");
+    table.string("title");
+    table.string("author");
+    table.string("summary");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("bookstores");
+  return knex.schema.dropTable("books");
 };
