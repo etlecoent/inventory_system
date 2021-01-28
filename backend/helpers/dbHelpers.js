c9b1ba26e9aff72488256d91b29e94cff8c8c5c0
Etienne LC <etlecoent@gmail.com>
2021-01-28 15:00:30 -0500

    Added function to get a specific book from a specific bookstore

--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -23,9 +23,42 @@ module.exports = (db) => {
       .catch((err) => console.log(err));
   };
 
+  const getBookstoreById = (id) => {
+    return db
+      .select("*")
+      .from("bookstores")
+      .where("id", id)
+      .then((result) => result[0])
+      .catch((err) => console.log(err));
+  };
+
+  const getBooksForBookstoreById = (id) => {
+    return db
+      .select("*")
+      .from("books")
+      .innerJoin("stored_books", "books.id", "stored_books.book_id")
+      .where("bookstore_id", id)
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  };
+
+  const getBookByIdForBookstoreById = (bookstore_id, book_id) => {
+    return db
+      .select("*")
+      .from("books")
+      .innerJoin("stored_books", "books.id", "stored_books.book_id")
+      .where("bookstore_id", bookstore_id)
+      .where("book_id", book_id)
+      .then((result) => result[0])
+      .catch((err) => console.log(err));
+  };
+
   return {
     getUsers,
     getBooks,
     getBookstores,
+    getBookstoreById,
+    getBooksForBookstoreById,
+    getBookByIdForBookstoreById,
   };
 };
