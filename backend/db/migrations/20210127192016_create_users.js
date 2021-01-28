f213a96d2740a9e77a9daa7ab798693a87d4db56
Etienne LC <etlecoent@gmail.com>
2021-01-27 20:25:16 -0500

    Added unicity on email & name

--- backend/db/migrations/20210127192016_create_users.js
+++ backend/db/migrations/20210127192016_create_users.js
@@ -6,6 +6,7 @@ exports.up = function (knex) {
     table.string("email");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.unique("email");
   });
 };
 
--- backend/db/migrations/20210127193126_create_bookstores.js
+++ backend/db/migrations/20210127193126_create_bookstores.js
@@ -4,6 +4,7 @@ exports.up = function (knex) {
     table.string("name");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.unique("name");
   });
 };
 
