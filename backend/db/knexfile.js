9531f0178688566928ed4659f4892dc5b6cad579
Etienne LC <etlecoent@gmail.com>
2021-01-27 19:57:15 -0500

    Used .env variables

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -1,21 +1,19 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
-const knexConfig = {
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
-  migrations: {
-    directory: "./migrations",
-  },
-  seeds: {
-    directory: "./seeds",
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
 };
-
-const knex = require("knex")(knexConfig);
-
-module.exports = knex;
