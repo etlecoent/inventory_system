ff19d7620e190ca6358d9f6bb5eeb8c166fee13e
Etienne LC <etlecoent@gmail.com>
2021-01-30 14:45:16 -0500

    Added not nullable to title, author, summary

--- backend/db/migrations/20210127201050_create_books.js
+++ backend/db/migrations/20210127201050_create_books.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("books", (table) => {
     table.increments("id").unsigned().primary();
-    table.string("title");
-    table.string("author");
-    table.string("summary");
+    table.string("title").notNullable();
+    table.string("author").notNullable();
+    table.string("summary").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
