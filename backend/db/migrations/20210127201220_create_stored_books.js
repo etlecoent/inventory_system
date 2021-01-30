e4f14d0e99d6d73bb046c4d9e789c13cc50e78ab
Etienne LC <etlecoent@gmail.com>
2021-01-30 11:07:30 -0500

    Added cascading & renamed table

--- backend/db/migrations/20210127201220_create_stored_books.js
+++ backend/db/migrations/20210127201220_create_stored_books.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("stored_books", (table) => {
+  return knex.schema.createTable("bookstores_books", (table) => {
     table.increments("id").unsigned().primary();
     table.integer("book_id").unsigned().notNullable();
     table.integer("bookstore_id").unsigned().notNullable();
@@ -7,12 +7,22 @@ exports.up = function (knex) {
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
 
-    table.foreign("book_id").references("id").inTable("books");
-    table.foreign("bookstore_id").references("id").inTable("bookstores");
+    table
+      .foreign("book_id")
+      .references("id")
+      .inTable("books")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table
+      .foreign("bookstore_id")
+      .references("id")
+      .inTable("bookstores")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
     table.unique(["book_id", "bookstore_id"]);
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("stored_books");
+  return knex.schema.dropTable("bookstores_books");
 };
