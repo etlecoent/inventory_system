f637005dbcaaa1a07a88bb5a8d87278ebdddc917
Etienne LC <etlecoent@gmail.com>
2021-01-30 12:01:57 -0500

    Added getById route

--- backend/routes/bookstoresBooksRoute.js
+++ backend/routes/bookstoresBooksRoute.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { ErrorHandler } = require("../helpers/errorsHelper");
 
-module.exports = ({ getBookstoresBooks, updateBookstoresBooks }) => {
+module.exports = ({
+  getBookstoresBooks,
+  getBookstoresBooksById,
+  updateBookstoresBooks,
+}) => {
   router.get("/", (req, res, next) => {
     getBookstoresBooks()
       .then((result) => {
@@ -11,12 +15,24 @@ module.exports = ({ getBookstoresBooks, updateBookstoresBooks }) => {
       .catch((err) => next(err));
   });
 
+  router.get("/:id", (req, res, next) => {
+    const { id } = req.params;
+    getBookstoresBooksById(id)
+      .then((result) => {
+        if (result.length) {
+          res.json(result[0]);
+        } else {
+          throw new ErrorHandler(404, "Not found");
+        }
+      })
+      .catch((err) => next(err));
+  });
+
   router.put("/", (req, res, next) => {
-    const {} = req.params;
     const { book_id, bookstore_id, quantity } = req.body;
 
     if (!book_id || !bookstore_id || !quantity) {
-      next(new ErrorHandler(400, "Missing field"));
+      next(new ErrorHandler(400, "Missing field(s)"));
     } else {
       updateBookstoresBooks(book_id, bookstore_id, quantity)
         .then((result) => {
@@ -29,6 +45,8 @@ module.exports = ({ getBookstoresBooks, updateBookstoresBooks }) => {
         .catch((err) => next(err));
     }
   });
+
+  router.delete("/", (req, res, next) => {});
 
   return router;
 };
