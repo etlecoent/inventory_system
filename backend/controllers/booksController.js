ec2dea6bcc0fb88149a6e71a5a12d84b1469ef84
Etienne LC <etlecoent@gmail.com>
2021-01-30 14:17:12 -0500

    Implemented routes from readme

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -7,7 +7,52 @@ module.exports = (db) => {
       .then((result) => result);
   };
 
+  const createBook = (title, author, summary) => {
+    return db("books")
+      .insert({
+        title,
+        author,
+        summary,
+      })
+      .returning("*")
+      .then((result) => result);
+  };
+
+  const getBookById = (id) => {
+    return db
+      .select("*")
+      .from("books")
+      .where("id", id)
+      .then((result) => result);
+  };
+
+  const deleteBookById = (id) => {
+    return db("books")
+      .where({ id })
+      .del()
+      .returning("*")
+      .then((result) => result);
+  };
+
+  const getBookstoresForBookById = (id) => {
+    return db
+      .select("*")
+      .from("bookstores")
+      .innerJoin(
+        "bookstores_books",
+        "bookstores.id",
+        "bookstores_books.bookstore_id"
+      )
+      .where("bookstore_id", id)
+      .orderBy("bookstores.id")
+      .then((result) => result);
+  };
+
   return {
     getBooks,
+    createBook,
+    getBookById,
+    deleteBookById,
+    getBookstoresForBookById,
   };
 };
--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -2,9 +2,76 @@ const express = require("express");
 const router = express.Router();
 const { ErrorHandler } = require("../helpers/errorsHelper");
 
-module.exports = ({ getBooks }) => {
-  router.get("/", (req, res) => {
-    getBooks().then((books) => res.json(books));
+module.exports = ({
+  getBooks,
+  createBook,
+  getBookById,
+  deleteBookById,
+  getBookstoresForBookById,
+}) => {
+  router.get("/", (req, res, next) => {
+    getBooks()
+      .then((books) => {
+        if (books.length) {
+          res.json(books);
+        } else {
+          throw new ErrorHandler(404, "Not found");
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+  router.post("/", (req, res, next) => {
+    const { title, author, summary } = req.body;
+
+    if (!title || !author || !summary) {
+      next(new ErrorHandler(400, "Missing field(s)"));
+    } else {
+      createBook(title, author, summary)
+        .then((result) => {
+          if (result.length) {
+            res.status(201).json(result[0]);
+          } else {
+            throw new ErrorHandler(403, "Already existing resource");
+          }
+        })
+        .catch((err) => next(err));
+    }
+  });
+
+  router.get("/:book_id", (req, res, next) => {
+    const { book_id } = req.params;
+    getBookById(book_id)
+      .then((result) => {
+        if (result.length) {
+          res.json(result[0]);
+        } else {
+          throw new ErrorHandler(404, "Not found");
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+  router.delete("/:books_id", (req, res, next) => {
+    const { books_id } = req.params;
+    deleteBookById(books_id)
+      .then((result) => {
+        if (result.length) {
+          res.status(202).json(result[0]);
+        } else {
+          throw new ErrorHandler(404, "Not found");
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+  router.get("/:books_id/bookstores", (req, res, next) => {
+    const { books_id } = req.params;
+    getBookstoresForBookById(books_id)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => next(err));
   });
 
   return router;
