b46409da7b3ba39e1eec9fca3c46c43e603ce271
Etienne LC <etlecoent@gmail.com>
2021-01-30 11:43:55 -0500

    Added put method

--- backend/routes/bookstoresBooksRoute.js
+++ backend/routes/bookstoresBooksRoute.js
@@ -2,13 +2,32 @@ const express = require("express");
 const router = express.Router();
 const { ErrorHandler } = require("../helpers/errorsHelper");
 
-module.exports = ({ getBookstoresBooks }) => {
+module.exports = ({ getBookstoresBooks, updateBookstoresBooks }) => {
   router.get("/", (req, res, next) => {
     getBookstoresBooks()
       .then((result) => {
         res.json(result);
       })
       .catch((err) => next(err));
+  });
+
+  router.put("/", (req, res, next) => {
+    const {} = req.params;
+    const { book_id, bookstore_id, quantity } = req.body;
+
+    if (!book_id || !bookstore_id || !quantity) {
+      next(new ErrorHandler(400, "Missing field"));
+    } else {
+      updateBookstoresBooks(book_id, bookstore_id, quantity)
+        .then((result) => {
+          if (result.length) {
+            res.json(result[0]);
+          } else {
+            res.status(501).json({ message: "Not found" });
+          }
+        })
+        .catch((err) => next(err));
+    }
   });
 
   return router;
