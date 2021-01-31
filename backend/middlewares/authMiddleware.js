aea0e38f72f88e3c7c699c8b49fbab4b13612ae5
Etienne LC <etlecoent@gmail.com>
2021-01-30 21:39:13 -0500

    Added custom error message when expired token

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -6,7 +6,9 @@ const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
 
   jwt.verify(authorization, process.env.JWT_KEY, (err, decoded) => {
-    if (err) {
+    if (err && err.message === "jwt expired") {
+      next(new ErrorHandler(401, "Session expired"));
+    } else if (err) {
       next(new ErrorHandler(401, "Unauthorized"));
     } else {
       next();
