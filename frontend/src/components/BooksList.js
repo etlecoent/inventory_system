d18568b32ee06766f4308849ec06c8d606b963d3
Etienne LC <etlecoent@gmail.com>
2021-02-01 15:29:26 -0500

    Added displaying cells in red when out of stock

--- frontend/src/components/BooksList.js
+++ frontend/src/components/BooksList.js
@@ -1,17 +1,38 @@
 import { useEffect, useState } from "react";
-import { DataGrid } from "@material-ui/data-grid";
+import { DataGrid, CellParams } from "@material-ui/data-grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import clsx from "clsx";
 import useAxios from "../hooks/useAxios";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    "& .negative": {
+      backgroundColor: "red",
+      fontWeight: "600",
+    },
+  },
+});
 
 const columns = [
-  { field: "book_id", headerName: "ID", width: 70 },
-  { field: "title", headerName: "Title", width: 300 },
-  { field: "author", headerName: "Author", width: 150 },
-  { field: "quantity", headerName: "Quantity", width: 120 },
+  { field: "book_id", headerName: "ID", width: 100 },
+  {
+    field: "quantity",
+    headerName: "Quantity",
+    width: 120,
+    cellClassName: (params: CellParams) =>
+      clsx("cell", {
+        negative: params.value <= 0,
+      }),
+  },
+  { field: "title", headerName: "Title", flex: 1 },
+  { field: "author", headerName: "Author", flex: 1 },
+  { field: "summary", headerName: "Summary", flex: 1 },
 ];
 
 export default function BooksList(props) {
   const { bookstoreId, token } = props;
+  const classes = useStyles();
   const axios = useAxios(token);
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -48,7 +69,12 @@ export default function BooksList(props) {
             height: "85vh",
             width: "100%",
           }}>
-          <DataGrid rows={books} columns={columns} pageSize={10} />
+          <DataGrid
+            rows={books}
+            columns={columns}
+            pageSize={10}
+            className={classes["root"]}
+          />
         </div>
       )}
     </>
